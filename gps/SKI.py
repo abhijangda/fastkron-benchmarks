082c6956cf0b8b936ed0b41aa0cbc527154a6a73
Abhinav Jangda <abhijangda@gmail.com>
2023-11-15 16:54:17 +0000

    single python file for gps

--- gps/SKI.py
+++ gps/SKI.py
@@ -5,166 +5,159 @@ import math
 import torch
 import time
 from scipy.io import loadmat
-
-# import set_managed_allocator
-
-# from matplotlib import pyplot as plt
 import numpy as np
-
-from torch.utils.data import TensorDataset, DataLoader
-
-from tqdm import tqdm
-#from sgkigp.gpyexample.gridkernel import CustomGridInterpolationKernel
-
-import switch_KroneckerProduct
 import gpytorch
-
-from gpytorch.kernels import GridInterpolationKernel
-
-# Source: https://docs.gpytorch.ai/en/latest/examples/02_Scalable_Exact_GPs/KISSGP_Regression.html
-
-import traceback
 import sys
 
-def format_exception(e):
-    exception_list = traceback.format_stack()
-    exception_list = exception_list[:-2]
-    exception_list.extend(traceback.format_tb(sys.exc_info()[2]))
-    exception_list.extend(traceback.format_exception_only(sys.exc_info()[0], sys.exc_info()[1]))
+KronMatmulTime = 0
 
-    exception_str = "Traceback (most recent call last):\n"
-    exception_str += "".join(exception_list)
-    # Removing the last \n
-    exception_str = exception_str[:-1]
+class SKI(gpytorch.models.ExactGP):
+  def __init__(self, train_x, train_y, likelihood, dims, grid_size):
+    super(SKI, self).__init__(train_x, train_y, likelihood)
+    self.mean_module = gpytorch.means.ConstantMean()
+    self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.GridInterpolationKernel(gpytorch.kernels.RBFKernel(ard_num_dims=dims),
+                                                    grid_size=grid_size, num_dims=dims))
 
-    return exception_str
+  def forward(self, x):
+    mean_x = self.mean_module(x)
+    covar_x = self.covar_module(x)
+    return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
-# n = int(sys.argv[1])
-# dims = int(sys.argv[2])
-grid_size = int(sys.argv[1])
-num_trace_samples = 10 #int(sys.argv[1])
+class LOVE(gpytorch.models.ExactGP):
+  class LargeFeatureExtractor(torch.nn.Sequential):           
+    def __init__(self, input_dim):                                      
+      super(LargeFeatureExtractor, self).__init__()        
+      self.add_module('linear1', torch.nn.Linear(input_dim, 1000))
+      self.add_module('relu1', torch.nn.ReLU())                  
+      self.add_module('linear2', torch.nn.Linear(1000, 500))     
+      self.add_module('relu2', torch.nn.ReLU())                  
+      self.add_module('linear3', torch.nn.Linear(500, 50))       
+      self.add_module('relu3', torch.nn.ReLU())                  
+      self.add_module('linear4', torch.nn.Linear(50, dims))         
 
-if False:
-    train_x = torch.zeros(n, dims)
-    # for i in range(n):
-    #     for j in range(dims):
-    #         train_x[i * dims + j] = float(i) / (n-1)
-    # True function is sin( 2*pi*(x0+x1))
-    train_y = torch.sin((train_x[:, 0] + train_x[:, 1]) * (2 * math.pi)) + torch.randn_like(train_x[:, 0]).mul(0.01)
-else:
-    dataset = sys.argv[2]
-    dataset_name = sys.argv[3]
-    data = torch.Tensor(loadmat(os.path.join(dataset,dataset_name+".mat"))['data'])
-    X = data[:, :-1]
-    X = X - X.min(0)[0]
-    X = 2 * (X / X.max(0)[0]) - 1
-    y = data[:, -1]
-    train_n = int(math.floor(0.64*len(X)))
-    train_x = X[:train_n, :].contiguous()
-    train_y = y[:train_n].contiguous()
-    dims = train_x.shape[1]
-    test_x = X[train_n:, :].contiguous()
-    test_y = y[train_n:].contiguous()
-
-print("N =", dims, "; P =", grid_size)
-train_x = train_x.cuda()
-train_y = train_y.cuda()
-
-class GPRegressionModel(gpytorch.models.ExactGP):
-    def __init__(self, train_x, train_y, likelihood):
-            super(GPRegressionModel, self).__init__(train_x, train_y, likelihood)
-
-            # SKI requires a grid size hyperparameter. This util can help with that
-
-            self.mean_module = gpytorch.means.ConstantMean()
-            # self.covar_module = gpytorch.kernels.ScaleKernel(
-            #     GridInterpolationKernel(
-            #         gpytorch.kernels.RBFKernel(), grid_size=grid_size, num_dims=dims
-            #     )
-            # )
-            
-            # self.covar_module = gpytorch.kernels.ProductStructureKernel(gpytorch.kernels.ScaleKernel(GridInterpolationKernel(gpytorch.kernels.RBFKernel(ard_num_dims=dims),
-            #                                             grid_size=grid_size, num_dims=1)), num_dims=dims)
-
-            self.covar_module = gpytorch.kernels.ScaleKernel(GridInterpolationKernel(gpytorch.kernels.RBFKernel(ard_num_dims=dims),
-                                                        grid_size=grid_size, num_dims=dims))
-            # if True:
-            #     self.covar_module = gpytorch.kernels.ProductStructureKernel(
-            #         gpytorch.kernels.ScaleKernel(GridInterpolationKernel(gpytorch.kernels.RBFKernel(ard_num_dims=dims),
-            #                                             grid_size=grid_size, num_dims=1)),
-            #         num_dims = dims
-            #     )
-
-    def forward(self, x):
-            mean_x = self.mean_module(x)
-            covar_x = self.covar_module(x)
-            return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+  def __init__(self, train_x, train_y, likelihood, dims, grid_size):
+    super(LOVE, self).__init__(train_x, train_y, likelihood)
     
-    # model = train_to_covergence(model, train_x, train_y)
+    self.mean_module = gpytorch.means.ConstantMean()
+    self.covar_module = gpytorch.kernels.GridInterpolationKernel(
+        gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel(ard_num_dims=dims)),
+        grid_size=grid_size, num_dims=dims,
+    )
+    
+    # Also add the deep net
+    self.feature_extractor = LargeFeatureExtractor(input_dim=train_x.size(-1))
 
-def train(training_iterations=5):
-    likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    model = GPRegressionModel(train_x, train_y, likelihood)
+  def forward(self, x):
+    # We're first putting our data through a deep net (feature extractor)
+    # We're also scaling the features so that they're nice values
+    projected_x = self.feature_extractor(x)
+    projected_x = projected_x - projected_x.min(0)[0]
+    projected_x = 2 * (projected_x / projected_x.max(0)[0]) - 1
+    
+    # The rest of this looks like what we've seen
+    mean_x = self.mean_module(projected_x)
+    covar_x = self.covar_module(projected_x)
+    return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
-    # Find optimal model hyperparameters
-    model.cuda()
-    likelihood.cuda()
+def train(klass, dataset, dataset_name, grid_size):
+  data = torch.Tensor(loadmat(os.path.join(dataset,dataset_name+".mat"))['data'])
+  X = data[:, :-1]
+  X = X - X.min(0)[0]
+  X = 2 * (X / X.max(0)[0]) - 1
+  y = data[:, -1]
+  train_n = int(math.floor(0.64*len(X)))
+  train_x = X[:train_n, :].contiguous()
+  train_y = y[:train_n].contiguous()
+  dims = train_x.shape[1]
+  test_x = X[train_n:, :].contiguous()
+  test_y = y[train_n:].contiguous()
 
-    model.train()
-    likelihood.train()
+  print("N =", dims, "; P =", grid_size)
+  train_x = train_x.cuda()
+  train_y = train_y.cuda()
 
-    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+  likelihood = gpytorch.likelihoods.GaussianLikelihood()
+  model = klass(train_x, train_y, likelihood, grid_size, dims)
 
-    # Use the adam optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
-    torch.cuda.synchronize()
-    import time
-    print('Tuning hyper-parameters ...')
-    start = time.time()
-    for i in range(training_iterations):
-        print ("iteration ", i)
-        # optimizer.zero_grad()
-        output = model(train_x)
-        # output@torch.ones(dims**grid_size, 11)
-        # try:
-        loss = -mll(output, train_y)
-        loss.backward()
-        # except torch.cuda.OutOfMemoryError as e:
-        #     import gc
-        #     print(format_exception(e))
-        #     for obj in gc.get_objects():
-        #         try:
-        #             if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
-        #                 print(type(obj), obj.size(), obj.numel() * obj.element_size())
-        #         except:
-        #             pass
-        #     return
-        optimizer.step()
-    torch.cuda.synchronize()
-    end = time.time()
-    print("Total time ", (end - start)*1e3, " ms")
-    print("KronMatmulTime ", KronMatmulTime, " ms")
-    # from torch.profiler import *
-    # with profile(activities=[ProfilerActivity.CUDA],
-    #     profile_memory=True, record_shapes=True) as prof:
-    #     try:
-    #         train()
-    #     except:
-    #         pass
+  # Find optimal model hyperparameters
+  model.cuda()
+  likelihood.cuda()
 
-    # print(prof.key_averages().table(sort_by="cuda_memory_usage", row_limit=10))
+  model.train()
+  likelihood.train()
 
-with gpytorch.settings.max_root_decomposition_size(0):
-    with gpytorch.settings.use_toeplitz(False):
-    # with  gpytorch.settings.fast_computations(, True, True):
-        with gpytorch.settings.num_trace_samples(num_trace_samples): 
-            with gpytorch.settings.fast_computations.log_prob(True):
-    # with gpytorch.settings.max_preconditioner_size(0):
-    # with gpytorch.settings.min_preconditioning_size(10):
-                with gpytorch.settings.debug(False):
-                    with gpytorch.settings.cg_tolerance(0):
-                        with gpytorch.settings.max_cholesky_size(0):
-                            with  gpytorch.settings.max_cg_iterations(20):
-                                # with  gpytorch.settings.terminate_cg_by_size(0):
-                                    with gpytorch.settings.verbose_linalg(True):
-                                        train()
+  mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+
+  # Use the adam optimizer
+  optimizer = torch.optim.Adam(model.parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
+  torch.cuda.synchronize()
+  start = time.time()
+  for i in range(training_iterations):
+    print ("i = ", i)
+    output = model(train_x)
+    loss = -mll(output, train_y)
+    loss.backward()
+    optimizer.step()
+  
+  torch.cuda.synchronize()
+  end = time.time()
+  print("Total time ", (end - start)*1e3, " ms")
+  print("KronMatmulTime ", KronMatmulTime, " ms")
+
+gpytorch.settings.max_root_decomposition_size(0)  
+gpytorch.settings.use_toeplitz(False)
+gpytorch.settings.fast_computations.log_prob(True)
+gpytorch.settings.debug(False)
+gpytorch.settings.cg_tolerance(0)
+gpytorch.settings.max_cholesky_size(0)
+gpytorch.settings.max_cg_iterations(20)
+gpytorch.settings.verbose_linalg(True)
+
+import linear_operator
+import linear_operator.operators.kronecker_product_linear_operator
+orig_matmul = linear_operator.operators.kronecker_product_linear_operator._matmul
+orig_t_matmul = linear_operator.operators.kronecker_product_linear_operator._t_matmul
+
+def switch_KroneckerProduct(use_fastkron):
+  global KronMatmulTime
+  
+  KronMatmulTime = 0
+  def new_matmul(linear_ops, kp_shape, rhs):
+      global KronMatmulTime
+      torch.cuda.synchronize()
+      s = time.time()
+      if use_fastkron:
+        res = rhs
+      else:
+        res = orig_matmul(linear_ops, kp_shape, rhs)
+      torch.cuda.synchronize()
+      e = time.time()
+      KronMatmulTime += (e - s) * 1000
+      return res
+
+  def new_t_matmul(linear_ops, kp_shape, rhs):
+      global KronMatmulTime
+      torch.cuda.synchronize()
+      s = time.time()
+      if use_fastkron:
+        res = rhs
+      else:
+        res = orig_t_matmul(linear_ops, kp_shape, rhs)
+      torch.cuda.synchronize()
+      e = time.time()
+      KronMatmulTime += (e - s) * 1000
+      return res
+
+  linear_operator.operators.kronecker_product_linear_operator._matmul = new_matmul
+  linear_operator.operators.kronecker_product_linear_operator._t_matmul = new_t_matmul
+
+if __name__ == "__main__":
+  gptype = sys.argv[1]
+  grid_size = int(sys.argv[2])
+  num_trace_samples = int(sys.argv[3])
+  dataset = sys.argv[4]
+  dataset_name = sys.argv[5]
+
+  with gpytorch.settings.num_trace_samples(num_trace_samples):
+    train(SKI, dataset, dataset_name, grid_size)
+  
